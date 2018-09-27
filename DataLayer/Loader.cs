73cf0edac0e3ef59d731ba3481596f381b6eb515
wrestlerdude <raishallan2@gmail.com>
2018-09-27 17:20:10 +0000

    Initialized DataLayer with abstract class

---
+++ DataLayer/Loader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace DataLayer
+{
+    abstract class Loader
+    {
+        private List<string> _raw_data;
+
+        public Loader(string file_path)
+        {
+            var csv_reader = new StreamReader(File.OpenRead(file_path));
+
+            if (csv_reader == null)
+                throw new ArgumentNullException("File Path");
+
+            while (!csv_reader.EndOfStream)
+            {
+                string curr = csv_reader.ReadLine();
+                _raw_data.Add(curr);
+            }
+        }
+    }
+}
