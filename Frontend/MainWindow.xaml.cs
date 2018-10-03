f5d42fb03b0de68d39442eb7942605c9cbb5d6f2
Hubert Krynicki <40336990@live.napier.ac.uk>
2018-10-03 15:04:59 +0100

    Revert "Heatmap frontend, problem with package"
    This reverts commit 9819d4a07554754c4095e6b946fa42fd6db0581e.

--- Frontend/MainWindow.xaml.cs
+++ Frontend/MainWindow.xaml.cs
@@ -28,7 +28,6 @@ namespace WpfApp1
         {
             InitializeComponent();
             myMap.CredentialsProvider = new Microsoft.Maps.MapControl.WPF.ApplicationIdCredentialsProvider(Backend.BackendStuff.KeyRequest());
-            myHeatMap.CredentialsProvider = new Microsoft.Maps.MapControl.WPF.ApplicationIdCredentialsProvider(Backend.BackendStuff.KeyRequest());
             myMap.Focus();
 
             if (!App.accessed)
@@ -163,25 +162,7 @@ namespace WpfApp1
         private void cboxShape_ContextMenuClosing(object sender, ContextMenuEventArgs e)
         {
         }
-                
-        private void btnHeatMap_Click(object sender, RoutedEventArgs e)
-        {
-            myHeatMap.Children.Clear();
-            
 
-            if (btnHeatMap.Content.Equals("Heat map"))
-            {
-                myMap.Visibility = Visibility.Hidden;
-                myHeatMap.Visibility = Visibility.Visible;
-                btnHeatMap.Content = "Map";
-
-            }
-            else
-            {
-                myHeatMap.Visibility = Visibility.Hidden;
-                myMap.Visibility = Visibility.Visible;
-                btnHeatMap.Content = "Heat map";
-            }
-        }
+        
     }
 }
